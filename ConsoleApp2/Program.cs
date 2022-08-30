c9012b904b36e0cd2e9b3784444e39177e2b92f0
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 11:58:57 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using NameEchoer;
+using System;
+
+namespace Names
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Echoer echoer = new Echoer();
+            EchoerAB echoerAB = new EchoerAB();
+
+            Console.WriteLine("Enter a program number");
+            string program = Console.ReadLine() ?? string.Empty;
+
+            switch (program)
+            {
+                case "1":
+                    string echoedName = echoer.EchoName();
+                    Console.WriteLine(echoedName);
+                    break;
+
+                case "2":
+                    string echoedAB = echoerAB.EchoNameAB();
+                    Console.WriteLine(echoedAB);
+                    break;
+
+                default:
+                    Console.WriteLine("Not a prorgram");
+                    break;
+            }
+        }
+    }
+}
---
+++ NameEcho/Echoer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameEchoer
+{
+    public class Echoer
+    {
+        public string EchoName()
+        {
+            Console.WriteLine("Please enter your name");
+
+            string name = Console.ReadLine() ?? string.Empty;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "User";
+            }
+
+            return $"Hello {name}";
+        }
+    }
+
+    public class EchoerAB
+    {
+        public string EchoNameAB()
+        {
+            Console.WriteLine("Please enter your name");
+
+            string name = Console.ReadLine() ?? string.Empty;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "User";
+            }
+
+            else if (name.ToLower() == "alice" || name.ToLower() == "bob")
+            {
+                return $"Hello {name}";
+            }
+
+            else
+            {
+                name = "User";
+            }
+
+            return $"Hello {name}";
+        }
+    }
+}
